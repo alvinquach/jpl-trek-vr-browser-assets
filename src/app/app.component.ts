6e76c57f9c1273d2e40631160992bbba22e2aec8
Unknown <alvinquach91@gmail.com>
2019-02-14 00:11:52 -0800

    trying things out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'jpl-trek-vr-browser-assets';
+
+  vals = {
+    a: 0,
+    b: 0,
+    c: 0
+  }
+
+  test(event, v) {
+    console.log(event);
+    this.vals[v] += 1;
+  }
+
+  keydown(event, v) {
+    console.log(event);
+  }
+
 }
